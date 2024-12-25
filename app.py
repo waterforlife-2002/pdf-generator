76b40ce24f0eee3e3fd9843c9f0d4f2ebdd2d670
Imran Rana <imranrana@iMac-von-Mo.local>
2024-12-25 12:22:19 +0100

    Update app to use 0.0.0.0 and dynamic port

--- app.py
+++ app.py
@@ -229,3 +229,8 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render gibt den Port über die Umgebungsvariable PORT an
+    app.run(host="0.0.0.0", port=port, debug=True)
